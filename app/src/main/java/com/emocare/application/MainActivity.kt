5d09e82882da3e5eb06c2a7fbea80aca4e95fa1b
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-10-30 23:10:58 +0700

    feat: membuat nav_bottom dan inisialisasi

--- app/src/main/java/com/emocare/application/MainActivity.kt
+++ app/src/main/java/com/emocare/application/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.emocare.application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.navhost_home) as NavHostFragment
+
+        binding.navBottom.setupWithNavController(navHost.navController)
     }
 }

a728d9d371a09296919130c98a1e7776cb3424de
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-12-05 16:26:43 +0700

    feat: gabung punya aul

--- app/src/main/java/com/emocare/application/adapter/DoctorAdapter.kt
+++ app/src/main/java/com/emocare/application/adapter/DoctorAdapter.kt
@@ -1,4 +1,4 @@
-package com.emocare.application
+package com.emocare.application.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.emocare.application.R
 
 
 data class Doctor(
